0934c84d81a4ae179f5c06b9aecc723ba872cb8b
mayankg3455 <mayankg3455@gmail.com>
2023-12-17 20:02:36 +0530

    added artists

--- src/genre.cpp
+++ src/genre.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 set<int> generateID;
-
+class Song;
 int generate_ID(int offset, int range)
 {
     srand((unsigned)time(NULL));
@@ -16,17 +16,19 @@ int generate_ID(int offset, int range)
     }
     return offset + (rand() % range);
 }
+map<string,list<Song* >> artistMap; // string as a key for artist name and artist ID as value
 
 class Song
 {
-    private:
+private:
     int songID;
     string title;
     vector<string> artists;
     int duration;
     string album;
-    public:
-    Song(){}
+
+public:
+    Song() {}
     Song(string title, vector<string> artists, int duration, string album)
     {
         // generate songID //
@@ -36,42 +38,83 @@ class Song
         this->duration = duration;
         this->album = album;
     }
+    // Adds song to their respective artists
+void addSongToArtists(map<string, list<Song* >>& artistMap,Song& song)
+{
+    for(string master : song.artists)
+    {
+        if(artistMap.find(master) == artistMap.end())
+        {
+            list<Song* > artistSongs;
+            artistSongs.push_back(&song);
+            artistMap[master] = artistSongs;
+        }
+        else
+        {
+            artistMap[master].push_back(&song);
+        }
+    }
+}
     void displaySong()
     {
-        cout << "Song ID : " << songID <<endl;
-        cout << "Title : " << title <<endl;
-        cout << "Artists :" <<endl;
+        cout << "Song ID : " << songID << endl;
+        cout << "Title : " << title << endl;
+        cout << "Artists :" << endl;
         int count = 0;
-        for(string s : artists)
+        for (string s : artists)
         {
-            
+
             cout << ++count << "." << s << endl;
         }
-        cout << "Duration : " << duration << "sec" <<endl;
+        cout << "Duration : " << duration << "sec" << endl;
         cout << "Album :" << album << endl;
-
-    }  
-
-};
-ostream& operator<<(ostream& COUT,Song& s)
-    {
-        s.displaySong();
-        return COUT;
     }
-class genre
+};
+ostream &operator<<(ostream &COUT, Song &s)
 {
-    private:
+    s.displaySong();
+    return COUT;
+}
+
+
+void displayArtistSongs(map<string, list<Song* >>& artistMap)
+{
+    for(auto it : artistMap)
+    {
+        cout << "Artist : " << it.first << endl;
+        int count = 0;
+        for(auto itr : it.second)
+        {
+            cout <<++count<<". "<< *(itr) << endl;
+        }
+        cout << "----------------------------------\n";
+    }
+}
+
+    // class Artist
+    // {
+    //     private:
+    //         // int artistID;
+    //         // string artistName;
+
+    //         // list<Song*> artistSong;
+
+    // };
+    class genre
+{
+private:
     int genreID;
     string genreName;
-    public:
-    list<Song*> genreSongs;
-    genre(){}
+
+public:
+    list<Song *> genreSongs;
+    genre() {}
     genre(string genreName)
     {
-        this->genreID = generate_ID(1,1500);
+        this->genreID = generate_ID(1, 1500);
         this->genreName = genreName;
     }
-    void addSongToGenre(Song& s)
+    void addSongToGenre(Song &s)
     {
         genreSongs.push_back(&s);
     }
@@ -80,37 +123,38 @@ class genre
         return genreName;
     }
 };
-ostream& operator<<(ostream& COUT,genre& Genre)
+ostream &operator<<(ostream &COUT, genre &Genre)
+{
+    COUT << "**********************************\n";
+    COUT << Genre.getGenreName() << endl;
+    for (Song *s : Genre.genreSongs)
     {
-        COUT << "**********************************\n";
-        COUT << Genre.getGenreName() << endl;
-        for(Song *s : Genre.genreSongs)
-        {
-            COUT << *s << endl;
-            COUT << "---------------------------------\n";
-        }
-        return COUT;
+        COUT << *s << endl;
+        COUT << "---------------------------------\n";
     }
+    return COUT;
+}
 /*
     The Library class contains a collection of Song objects.
     Library class serving as a repository or collection of available songs.
     Song class instances exist independently, and the Library class manages a collection of these songs.
     Users or other parts of the system can interact with the Library to access and manage available songs.
 */
-struct Library
+class Library
 {
-    list<genre*> genreList;
+public:
+    list<genre *> genreList;
     // list<Song*> songList;
-    void addGenreToLibrary(genre& Genre)
+    void addGenreToLibrary(genre &Genre)
     {
         genreList.push_back(&Genre);
     }
 
-}lib;
-ostream& operator<<(ostream& COUT,Library& lib)
+} lib;
+ostream &operator<<(ostream &COUT, Library &lib)
 {
     COUT << "LIST OF ALL SONGS " << endl;
-    for(genre *Genre : lib.genreList)
+    for (genre *Genre : lib.genreList)
     {
         COUT << *Genre << endl;
     }
@@ -119,17 +163,24 @@ ostream& operator<<(ostream& COUT,Library& lib)
 int main()
 {
     Song s1 = Song("Mitwa", {"Shafqat Amanat Ali", "Shankar Mahadevan", "Caralisa Monteiro"}, 383, "Kabhi Alvida Naa Kehna");
+    s1.addSongToArtists(artistMap,s1);
     Song s2 = Song("Saware", {"Arijit Singh"}, 383, "Phantom");
-    Song s3("Dancing in the Moonlight", {"King Harvest"}, 200, "Old Friends");
-    Song s4("Havana", {"Camila Cabello", "Young Thug"}, 217, "Camila");
-    genre indianPop = genre("India Film Pop");
-    genre latinPop = genre("Latin Pop");
-    indianPop.addSongToGenre(s1);
-    indianPop.addSongToGenre(s2);
-    latinPop.addSongToGenre(s3);
-    latinPop.addSongToGenre(s4);
-    lib.addGenreToLibrary(indianPop);
-    lib.addGenreToLibrary(latinPop);
-    cout << lib;
+    s2.addSongToArtists(artistMap,s2);
+    // Song s3("Dancing in the Moonlight", {"King Harvest"}, 200, "Old Friends");
+    // Song s4("Havana", {"Camila Cabello", "Young Thug"}, 217, "Camila");
+    Song s5("Tum Hi Ho", {"Arijit Singh"}, 275, "Aashiqui 2");
+    s5.addSongToArtists(artistMap,s5);
+    // genre indianPop = genre("India Film Pop");
+    // genre latinPop = genre("Latin Pop");
+    // indianPop.addSongToGenre(s1);
+    // indianPop.addSongToGenre(s2);
+    // latinPop.addSongToGenre(s3);
+    // latinPop.addSongToGenre(s4);
+    // lib.addGenreToLibrary(indianPop);
+    // lib.addGenreToLibrary(latinPop);
+    // cout << lib;
+    
+    displayArtistSongs(artistMap);
+
     return 0;
 }
